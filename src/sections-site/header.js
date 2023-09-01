c2b3d86ac959e57677c2ff2461fca89685e2154d
CleProgrammer <clebsonmengao@hotmail.com>
2023-09-01 15:10:53 -0300

    changes

--- src/sections-site/header.js
+++ src/sections-site/header.js
@@ -1,8 +1,11 @@
 import './header.css'
 import $ from 'jquery'
 import LogoCB from '../images/logos/cb-logo-blue.png'
+import menuOpened from '../images/openmenumobilerotate.png'
+import menuClosed from '../images/openmenumobile.png'
 
 export default function Header() {
+    const c = (cl) => document.querySelector(cl)
 
     function sitePlace(e) {
         if( e.target.id === 'home' ) {
@@ -28,11 +31,33 @@ export default function Header() {
         }
     }
 
+    function openMobileMenu(e) {
+        if( e.target.id === 'disable' ) {
+            c('.menu-mobile').style.backgroundImage = 'url('+`${menuOpened}`+')'
+            c('.menu-mobile ul').style.display = 'block'
+            c('.menu-mobile ul').style.position = 'absolute'
+            e.target.id = 'enable'
+        } else {
+            c('.menu-mobile').style.backgroundImage = 'url('+`${menuClosed}`+')'
+            c('.menu-mobile ul').style.display = 'none'
+            e.target.id = 'disable'
+        }
+    }
+
     return (
         <div className="header-main">
             <div className='box-header1'><img src={LogoCB}/></div>
             <div className='box-header2'>
-                <ul style={{listStyleType: 'none'}}>
+                <div id='disable' className='menu-mobile' onClick={openMobileMenu}>
+                    <ul style={{listStyleType: 'none'}}>
+                        <li id='home' onClick={sitePlace}>HOME</li>
+                        <li id='aboutme' onClick={sitePlace}>SOBRE MIM</li>
+                        <li id='cert' onClick={sitePlace}>CERTIFICADOS</li>
+                        <li id='projects' onClick={sitePlace}>PROJETOS</li>
+                        <li id='contacts' onClick={sitePlace}>CONTATOS</li>
+                    </ul>
+                </div>
+                <ul className='menu-desktop' style={{listStyleType: 'none'}}>
                     <li id='home' onClick={sitePlace}>HOME</li>
                     <li id='aboutme' onClick={sitePlace}>SOBRE MIM</li>
                     <li id='cert' onClick={sitePlace}>CERTIFICADOS</li>
